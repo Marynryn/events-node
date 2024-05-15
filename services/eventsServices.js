29374b8b56e87506216f69e9c3d0e86d6a25fd91
Marynryn <murmar95@gmail.com>
2024-05-15 22:32:09 +0300

    body

--- services/eventsServices.js
+++ services/eventsServices.js
@@ -5,7 +5,11 @@ export const getEventsList = () => Event.find();
 export const getEventById = (id) => Event.findOne({ _id: id });
 
 export const signup = async (id, body) => {
-  const event = await Event.findOneAndUpdate({ _id: id }, body, { new: true });
+  const event = await Event.findByIdAndUpdate(
+    { _id: id },
+    { $push: body },
+    { new: true }
+  );
 
   return event.participants;
 };
